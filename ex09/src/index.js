80710ebf66d68f69c5cd8c3e5d5e4336fae49c60
Teska98 <63234508+Teska98@users.noreply.github.com>
2021-03-12 00:28:05 +0100

    Create index.js

---
+++ ex09/src/index.js
@@ -0,0 +1,17 @@
+function main(){
+    // Only change code below this line
+     var firstNum = 6;
+     var secondNum = 25;
+     var thirdNum = 5.2;
+     myX *= 9;
+     myY /= 5;
+     myZ *= 10;
+    // Only chane code above this line
+ return {
+   firstNum,
+   seconNum,
+   thirdNum
+ };
+}
+console.log(main());
+module.exports = main;
