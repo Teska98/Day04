7d30b50224d232dba4361c2b1cb160d74410595c
Teska98 <63234508+Teska98@users.noreply.github.com>
2021-03-11 23:55:51 +0100

    Create index.js

---
+++ ex07/src/index.js
@@ -0,0 +1,11 @@
+function main(numOne, numTwo){
+    // Only change code below this line
+     var remainder;
+     var numOne = 15;
+     var numTwo = 4;
+     remainder = numOne % numTwo;
+    // Only chane code above this line
+ return remainder;
+}
+console.log(main(15, 4));
+module.exports = main;
