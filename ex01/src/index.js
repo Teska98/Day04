da917cf799123c7037ab1f40de767734e6992cbb
Teska98 <63234508+Teska98@users.noreply.github.com>
2021-03-11 12:34:17 +0100

    Create index.js

---
+++ ex01/src/index.js
@@ -0,0 +1,19 @@
+function main(numOne, numTwo){
+
+  var quotient;
+  if(numTwo === 0){
+    quotient = "Devide by 0! Cannot divide by zero!";
+  } else{
+    // Only change code below this line
+       var quotient
+       var numOne = 15;
+       var numTwo = 3;
+       quotient = numOne / numTwo;
+    // Only chane code above this line
+  }
+  
+  return quotient;
+}
+
+console.log(main(15, 3));
+module.exports = main;
