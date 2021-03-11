4361cc746c0e9d04aab4d9cd08d345febbbd8a04
Teska98 <63234508+Teska98@users.noreply.github.com>
2021-03-11 23:26:34 +0100

    Create index.js

---
+++ ex03/src/index.js
@@ -0,0 +1,11 @@
+function main(){
+
+  var myRes = 16;
+    // Only change code below this line
+       myRes--;
+    // Only chane code above this line
+
+}
+
+console.log(main());
+module.exports = main;
