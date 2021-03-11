fb5371e90b003bee1f03f454803be218b165bc6e
Teska98 <63234508+Teska98@users.noreply.github.com>
2021-03-11 23:44:50 +0100

    Create index.js

---
+++ ex05/src/index.js
@@ -0,0 +1,11 @@
+function main(numOne, numTwo){
+    // Only change code below this line
+     var product;
+     var numOne = 5.5;
+     var numTwo = 5.5;
+     product = numOne * numTwo;
+    // Only chane code above this line
+ return product;
+}
+console.log(main(5.5, 5.5));
+module.exports = main;
