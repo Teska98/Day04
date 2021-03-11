89f2a51a7c6044cc2cba4c03295120674bc51076
Teska98 <63234508+Teska98@users.noreply.github.com>
2021-03-11 23:48:33 +0100

    Create index.js

---
+++ ex06/src/index.js
@@ -0,0 +1,11 @@
+function main(numOne, numTwo){
+    // Only change code below this line
+     var myQuotient;
+     var numOne = 16.5;
+     var numTwo = 5.5;
+     myQuotient = numOne / numTwo;
+    // Only chane code above this line
+ return myQuotient;
+}
+console.log(main(16.5, 5.5));
+module.exports = main;
