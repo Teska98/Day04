3489a7615c310f4a9ea29004611a7c21649054b7
Teska98 <63234508+Teska98@users.noreply.github.com>
2021-03-12 00:33:24 +0100

    Create index.js

---
+++ ex10/src/index.js
@@ -0,0 +1,14 @@
+function main(){
+    // Only change code below this line
+     var myFirstName = "Edvin";
+     var myLastName = "Teskeredžić";
+     var myHomeTown = "Sarajevo";
+   // Only chane code above this line
+ return {
+   myFirstName,
+   myLastName,
+   myHomeTown
+ };
+}
+console.log(main());
+module.exports = main;
